c7edce791b26084c33cd57e73a3a7f6ef676e49a
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-05-15 15:11:19 +0200

    commenti al codice nel main js

--- main.js
+++ main.js
@@ -4,21 +4,18 @@ createApp({
 
     data() {
         return {
-            mail: null,
             indirizzo: 'https://flynn.boolean.careers/exercises/api/random/mail',
-            result : this
+            result : this //se ho capito bene mettendo questo io posso chiamare il "this" nelle funzioni senza arrow, da chiedere perché mi funziona anche senza
         }
     },
     methods: {
         
         getMail() {
-            axios.get(this.indirizzo).then((result) => { //attenzione all'arrow function!!
+            axios.get(this.indirizzo).then(function(result) { //attenzione all'arrow function!!
 
-                let datiRisposta = result.data;
+                let datiRisposta = result.data; //questo fa riferimento al data() a riga 5, esattamente quella funzione, ma non capisco come faccia riferimento all'elemento che restituisce... posso immaginare solo che dipende dalla variabile indirizzo, quindi la funzione è scritta lì che restituisce un email casuale
 
                 console.log("Ricevuta risposta", datiRisposta);
-
-                this.mail = datiRisposta.mail;
 
             });
         }
