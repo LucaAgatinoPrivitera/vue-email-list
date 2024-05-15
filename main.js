6d4140c5ed5ecc8768807e2065ba7d6a885c401a
LucaAgatinoPrivitera <priviteraluca01@gmail.com>
2024-05-15 16:12:02 +0200

    Non gira la creazione dei vari elementi

--- main.js
+++ main.js
@@ -5,19 +5,26 @@ createApp({
     data() {
         return {
             indirizzo: 'https://flynn.boolean.careers/exercises/api/random/mail',
-            result : this //se ho capito bene mettendo questo io posso chiamare il "this" nelle funzioni senza arrow, da chiedere perché mi funziona anche senza
+            result: this, //se ho capito bene mettendo questo io posso chiamare il "this" nelle funzioni senza arrow, da chiedere perché mi funziona anche senza
+            mailRicevute : []
         }
     },
+
     methods: {
-        
         getMail() {
-            axios.get(this.indirizzo).then(function(result) { //attenzione all'arrow function!!
+            for (let i = 0; i <= 10; i++) {
+                axios.get(this.indirizzo).then((result) => { //attenzione all'arrow function!!
 
-                let datiRisposta = result.data; //questo fa riferimento al data() a riga 5, esattamente quella funzione, ma non capisco come faccia riferimento all'elemento che restituisce... posso immaginare solo che dipende dalla variabile indirizzo, quindi la funzione è scritta lì che restituisce un email casuale
+                    let datiRisposta = result.data; //questo fa riferimento al data() a riga 5, esattamente quella funzione, ma non capisco come faccia riferimento all'elemento che restituisce... posso immaginare solo che dipende dalla variabile indirizzo, quindi la funzione è scritta lì che restituisce un email casuale
+    
+                    console.log("Ricevuta risposta", datiRisposta);
+                
+                    this.mailRicevute.push(this.datiRisposta);
+                    return datiRisposta;
+                });
+                
+            }
 
-                console.log("Ricevuta risposta", datiRisposta);
-
-            });
         }
     },
     mounted() {
